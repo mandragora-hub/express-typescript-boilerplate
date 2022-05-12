aad994dc3306db3f23d4b0deaa1e81d5109a72c2
mandragora-hub <royer01gomez@gmail.com>
2022-05-11 19:42:43 -0500

    fix non relative module resolver

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# express-typesript-boilerplate
-express-typesript-boilerplate
+# express-typescript-boilerplate
+express-typescript-boilerplate
+
+
+
+
+
+
+Credits: 
+
+* [Avoid Relative Path Hell in TypeScript Backend](https://medium.com/geekculture/avoid-relative-path-hell-in-typescript-backend-41417b0086b7)
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { Express, Request, Response } from 'express'
+import v1Router from './v1/routes/workoutRoutes'
+
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -9,6 +11,8 @@ const port = process.env.PORT
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server')
 })
+
+app.get('/api/v1', v1Router)
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
