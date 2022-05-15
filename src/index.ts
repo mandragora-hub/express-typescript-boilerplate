bef5ee6997a639fc8181a017ccc85f42437e1079
mandragora-hub <royer01gomez@gmail.com>
2022-05-14 19:23:09 -0500

    workout section ready

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express'
-import v1Router from './v1/routes/workoutRoutes'
+import ErrorHandler from './commons/errorHandler'
+import v1RouterWorkout from './v1/routes/workoutRoutes'
 
 import dotenv from 'dotenv'
 
@@ -8,11 +9,16 @@ dotenv.config()
 const app: Express = express()
 const port = process.env.PORT
 
+app.use(express.json())
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server')
 })
 
-app.get('/api/v1', v1Router)
+app.use('/api/v1/workout', v1RouterWorkout)
+
+app.use(ErrorHandler.logError)
+app.use(ErrorHandler.sendError)
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
