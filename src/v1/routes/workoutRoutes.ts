2edde9d77ffbfdfb290cd7b7156c032fae7044d0
mandragora-hub <royer01gomez@gmail.com>
2022-05-14 22:12:22 -0500

    added cache to getallworkout route

--- src/v1/routes/workoutRoutes.ts
+++ src/v1/routes/workoutRoutes.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import workoutController from 'src/controllers/workoutController'
 import recordController from 'src/controllers/recordController'
+import apicache from 'apicache'
 
+const cache = apicache.middleware;
 
 const router = express.Router();
 
-router.get("/", workoutController.getAllWorkouts);
+router.get("/", cache('2 minutes'), workoutController.getAllWorkouts);
 
 router.get("/:workoutId", workoutController.getOneWorkout);
 
