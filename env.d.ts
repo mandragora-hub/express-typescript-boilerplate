ff8d396a588243709d24abb9b81b8b857fae3b26
mandragora-hub <royer01gomez@gmail.com>
2022-06-04 17:45:31 -0500

    .

--- env.d.ts
+++ env.d.ts
@@ -5,11 +5,12 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       HTTP_PORT: number;
-      NODE_ENV: 'development' | 'test';
+      NODE_ENV: 'development' | 'test' | 'production';
       
-      CI_DB_PASSWORD: string;
       CI_DB_NAME: string;
+      CI_DB_HOST: string;
       CI_DB_USERNAME: string;
+      CI_DB_PASSWORD: string;
       //   DB_PORT: number;
     }
   }
